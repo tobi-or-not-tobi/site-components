9777a38feb9fd85ce88055431ccb50ed0dc88d14
Tobias Ouwejan <tobiasouwejan@gmail.com>
2018-06-06 23:19:40 +0200

    Use a small bounceRate for recalculate on window resize

--- projects/event-components/src/lib/countdown/countdown/countdown.component.ts
+++ projects/event-components/src/lib/countdown/countdown/countdown.component.ts
@@ -1,17 +1,10 @@
-import {
-    Component,
-    ChangeDetectionStrategy,
-    Input,
-    ElementRef,
-    HostListener,
-    OnChanges,
-    AfterViewInit,
-    ViewEncapsulation
-} from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, ElementRef, OnChanges, AfterViewInit } from '@angular/core';
 import { TimerService } from '../timer.service';
 import { CountDownModule } from 'event-components';
 import { CountdownType } from '../countdown.model';
+import { Observable, fromEvent } from 'rxjs';
 
+import { debounceTime, map } from 'rxjs/operators';
 @Component({
     selector: 'lib-countdown',
     templateUrl: './countdown.component.html',
@@ -22,15 +15,19 @@ export class CountdownComponent implements AfterViewInit, OnChanges {
     @Input() startDate: Date;
     @Input() types: CountdownType[];
 
-    @HostListener('window:resize', ['$event'])
-    onResize(event) {
+    hostHeight: number;
+
+    constructor(private element: ElementRef, private timer: TimerService) {
+        fromEvent(window, 'resize')
+            .pipe(debounceTime(50))
+            .subscribe(() => this.calcWidth());
+    }
+
+    ngAfterViewInit() {
+        this.hostHeight = parseInt(window.getComputedStyle(this.element.nativeElement).height);
         this.calcWidth();
     }
 
-    constructor(private element: ElementRef, private timer: TimerService) {}
-    ngAfterViewInit() {
-        this.calcWidth();
-    }
     ngOnChanges() {
         this.timer.initTimer(this.startDate);
     }
@@ -40,12 +37,11 @@ export class CountdownComponent implements AfterViewInit, OnChanges {
         // if there's not a height given, or if it doesn't fit, we use the width
         const fullWidth = parseInt(window.getComputedStyle(this.element.nativeElement).width);
         const circleWidth = Math.floor(fullWidth / this.types.length);
-        const height = parseInt(window.getComputedStyle(this.element.nativeElement).height);
 
-        if (height > 0 && circleWidth >= height) {
-            this.element.nativeElement.style.setProperty('--aspect-ratio', Math.floor(height));
+        if (this.hostHeight > 0 && circleWidth >= this.hostHeight) {
+            this.element.nativeElement.style.setProperty('--aspect-ratio', Math.floor(this.hostHeight));
         } else {
-            this.element.nativeElement.style.setProperty('--aspect-ratio', Math.floor(fullWidth / this.types.length));
+            this.element.nativeElement.style.setProperty('--aspect-ratio', circleWidth);
         }
     }
 }
